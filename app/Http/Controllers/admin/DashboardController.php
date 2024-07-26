f0e736eae5680fcc97de58969e87f7b4a95fd837
ikhsannwwi <ikhsannawawi99@gmail.com>
2024-07-26 22:51:18 +0700

    revisi

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -40,23 +40,23 @@ class DashboardController extends Controller
 
     protected function statisticMonthly() {
         // Get data for the past 12 months of the current year
-        $monthlyStatistics = Statistic::selectRaw('TO_CHAR(visit_time, \'YYYY-MM\') as month, COUNT(*) as count')
+        $monthlyStatistics = Statistic::selectRaw("DATE_FORMAT(visit_time, '%Y-%m') as month, COUNT(*) as count")
             ->whereYear('visit_time', now()->year)
-            ->groupByRaw('month')
+            ->groupByRaw('DATE_FORMAT(visit_time, \'%Y-%m\')')
             ->orderBy('month', 'desc')
             ->take(12) // Display data for the past 12 months
             ->get();
-    
+        
         // Create an array with the labels for the past 12 months
         $chartLabelsMonthly = [];
         for ($i = 11; $i >= 0; $i--) {
             $date = now()->subMonths($i);
             $chartLabelsMonthly[] = $date->format('M') . ' (0)';
         }
-    
+        
         // Initialize data array with zeros
         $chartDataMonthly = array_fill(0, 12, 0);
-    
+        
         // Fill in data for existing months
         foreach ($monthlyStatistics as $month) {
             $index = array_search(date('M', strtotime($month->month)) . ' (0)', $chartLabelsMonthly);
@@ -65,11 +65,12 @@ class DashboardController extends Controller
                 $chartLabelsMonthly[$index] = date('M', strtotime($month->month)) . ' (' . $month->count . ')';
             }
         }
-    
+        
         return [
             'chartData'   => $chartDataMonthly,
             'chartLabels' => $chartLabelsMonthly,
         ];
     }
+    
     
 }
